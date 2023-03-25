f4d69029ac5dd6a44aab7886d429e68c379db95b
Nuno Silva <little.coding.fox@gmail.com>
2023-03-25 11:24:55 +0000

    Fixes to bgfx DLL name on cross platform

--- Engine/Core/Bindings/bgfx_dllname.cs
+++ Engine/Core/Bindings/bgfx_dllname.cs
@@ -1,7 +1,15 @@
+using System.Runtime.InteropServices;
+
 namespace Bgfx
 {
     public static partial class bgfx
     {
+        #if WINDOWS
         const string DllName = "bgfx.dll";
+        #elif OSX
+        const string DllName = "libbgfx.dylib";
+        #elif LINUX
+        const string DllName = "libbgfx.so";
+        #endif
     }
 }
