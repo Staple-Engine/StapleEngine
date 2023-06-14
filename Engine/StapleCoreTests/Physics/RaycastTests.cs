223c9b90be712f1e70c54296a3e5eb3b81f96a8f
Nuno Silva <little.coding.fox@gmail.com>
2023-06-14 17:36:36 +0100

    Fix for Raycast Tests;

--- Engine/StapleCoreTests/Physics/RaycastTests.cs
+++ Engine/StapleCoreTests/Physics/RaycastTests.cs
@@ -8,13 +8,17 @@ namespace CoreTests
         [Test]
         public void TestBoxRaycast()
         {
+            LayerMask.AllLayers.Add("Default");
+
             Physics3D.Instance = new Physics3D(new JoltPhysics3D());
 
             Physics3D.Instance.Startup();
 
             var ray = new Ray(new Vector3(0, 0, 1), new Vector3(0, 0, -1));
 
-            Assert.IsTrue(Physics3D.Instance.CreateBox(Entity.Empty, Vector3.One, Vector3.Zero, Quaternion.Identity, BodyMotionType.Dynamic, 0, out var body));
+            Assert.IsTrue(Physics3D.Instance.CreateBox(Entity.Empty, Vector3.One * 2, Vector3.Zero, Quaternion.Identity, BodyMotionType.Dynamic, 0, out var body));
+
+            body.GravityFactor = 0;
 
             Assert.IsTrue(Physics.RayCast3D(ray, out var target, out _));
 
