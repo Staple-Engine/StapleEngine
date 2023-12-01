bc4a99ae7c217f28193d0d621c434f9e734388cd
Nuno Silva <little.coding.fox@gmail.com>
2023-12-01 14:40:48 +0000

    Added ObservableBox/Manager;

---
+++ Engine/Core/Utilities/ObservableBox.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Staple.Internal;
+
+internal class ObservableBox
+{
+    class ObserverInfo
+    {
+        public Assembly assembly;
+        public WeakReference<object> observer;
+    }
+
+    private List<ObserverInfo> observers = new();
+    private List<object> stagingObservers = new();
+    private object observerLock = new();
+
+    public ObservableBox()
+    {
+        ObservableManager.Add(this);
+    }
+
+    public void RemoveAll(Assembly assembly)
+    {
+        lock(observerLock)
+        {
+            for(var i = observers.Count - 1; i >= 0; i--)
+            {
+                if (observers[i].assembly == assembly)
+                {
+                    observers.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    public void AddObserver(object type)
+    {
+        lock(observerLock)
+        {
+            foreach(var item in observers)
+            {
+                if(item.observer.TryGetTarget(out var o) && o == type)
+                {
+                    return;
+                }
+            }
+
+            observers.Add(new ObserverInfo()
+            {
+                assembly = type.GetType().Assembly,
+                observer = new WeakReference<object>(type),
+            });
+        }
+    }
+
+    public void RemoveObserver(object type)
+    {
+        lock (observerLock)
+        {
+            foreach (var item in observers)
+            {
+                if (item.observer.TryGetTarget(out var o) && o == type)
+                {
+                    observers.Remove(item);
+
+                    return;
+                }
+            }
+        }
+    }
+
+    public void Emit(Action<object> callback)
+    {
+        lock(observerLock)
+        {
+            for(var i = observers.Count - 1; i >= 0; i--)
+            {
+                var item = observers[i];
+
+                if (item.observer.TryGetTarget(out var o))
+                {
+                    stagingObservers.Add(o);
+                }
+                else
+                {
+                    observers.RemoveAt(i);
+                }
+            }
+
+            foreach (var observer in stagingObservers)
+            {
+                callback(observer);
+            }
+
+            stagingObservers.Clear();
+        }
+    }
+}
---
+++ Engine/Core/Utilities/ObservableManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Staple.Internal;
+
+internal static class ObservableManager
+{
+    private static List<WeakReference<ObservableBox>> boxes = new();
+    private static object lockObject = new();
+
+    public static void RemoveAll(Assembly assembly)
+    {
+        lock (lockObject)
+        {
+            for(var i = boxes.Count - 1; i >= 0; i--)
+            {
+                var box = boxes[i];
+
+                if(box.TryGetTarget(out var c))
+                {
+                    c.RemoveAll(assembly);
+                }
+            }
+        }
+    }
+
+    public static void Add(ObservableBox box)
+    {
+        lock(lockObject)
+        {
+            foreach(var b in boxes)
+            {
+                if(b.TryGetTarget(out var c) && c == box)
+                {
+                    return;
+                }
+            }
+
+            boxes.Add(new(box));
+        }
+    }
+
+    public static void Remove(ObservableBox box)
+    {
+        lock (lockObject)
+        {
+            for(var i = boxes.Count - 1; i >= 0; i--)
+            {
+                var b = boxes[i];
+
+                if(b.TryGetTarget(out var c) && c == box)
+                {
+                    boxes.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
