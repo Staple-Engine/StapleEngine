e08843db01040dfb86e905d1066ce3eb226f565a
Nuno Silva <little.coding.fox@gmail.com>
2024-01-30 23:36:59 +0000

    [Editor] Add WIP support for macOS .app bundles;

--- Engine/Editor/StapleEditor+Build.cs
+++ Engine/Editor/StapleEditor+Build.cs
@@ -3,6 +3,7 @@ using Staple.Internal;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Staple.Editor
 {
@@ -166,7 +167,7 @@ namespace Staple.Editor
 
             if(backend.publish)
             {
-                args = $" publish -r {backend.platformRuntime} \"{projectPath}\" -c {configurationName} -o \"{outPath}\" --self-contained";
+                args = $" publish -r {backend.platformRuntime} \"{projectPath}\" -c {configurationName} -o \"{outPath}\" --self-contained -p:UseAppHost=true";
             }
             else
             {
@@ -225,6 +226,161 @@ namespace Staple.Editor
             }
             catch (Exception)
             {
+            }
+
+            if(backend.platform == AppPlatform.MacOSX)
+            {
+                var appName = Path.GetFileName(outPath);
+
+                var appPath = Path.Combine(outPath, $"{appName}.app");
+
+                try
+                {
+                    Directory.CreateDirectory(appPath);
+                }
+                catch(Exception)
+                {
+                }
+
+                try
+                {
+                    Directory.CreateDirectory(Path.Combine(appPath, "Contents"));
+                }
+                catch (Exception)
+                {
+                }
+
+                try
+                {
+                    Directory.CreateDirectory(Path.Combine(appPath, "Contents", "MacOS"));
+                }
+                catch (Exception)
+                {
+                }
+
+                try
+                {
+                    Directory.CreateDirectory(Path.Combine(appPath, "Contents", "MacOS", "Data"));
+                }
+                catch (Exception)
+                {
+                }
+
+                var info = $$"""
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>CFBundleDevelopmentRegion</key>
+    <string>English</string>
+    <key>CFBundleExecutable</key>
+    <string>Run</string>
+    <key>CFBundleGetInfoString</key>
+    <string>Staple Engine - {{appName}} {{projectAppSettings.appDisplayVersion}}</string>
+    <key>CFBundleIconFile</key>
+    <string>Icon.icns</string>
+    <key>CFBundleIdentifier</key>
+    <string>{{projectAppSettings.appBundleID}}</string>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
+    <key>CFBundleName</key>
+    <string>{{projectAppSettings.appName}}</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleShortVersionString</key>
+    <string>{{projectAppSettings.appDisplayVersion}}</string>
+    <key>CFBundleSupportedPlatforms</key>
+    <array>
+      <string>MacOSX</string>
+    </array>
+    <key>CFBundleVersion</key>
+    <string>{{projectAppSettings.appVersion}}</string>
+    <key>LSApplicationCategoryType</key>
+    <string>public.app-category.games</string>
+    <key>LSMinimumSystemVersion</key>
+    <string>10.13.0</string>
+    <key>NSHighResolutionCapable</key>
+    <true/>
+    <key>NSAppTransportSecurity</key>
+    <dict>
+      <key>NSAllowsArbitraryLoads</key>
+      <true />
+    </dict>
+  </dict>
+</plist>
+""";
+
+                try
+                {
+                    File.WriteAllText(Path.Combine(appPath, "Contents", "Info.plist"), info);
+                }
+                catch(Exception e)
+                {
+                    Log.Debug(e.ToString());
+                }
+
+                var runScript = $$"""
+#!/bin/sh
+
+cd $(dirname $BASH_SOURCE)
+./{{appName}}
+""";
+
+                try
+                {
+                    var p = Path.Combine(appPath, "Contents", "MacOS", "Run");
+
+                    File.WriteAllText(p, runScript);
+
+                    if(OperatingSystem.IsLinux() ||
+                        OperatingSystem.IsMacOS() ||
+                        OperatingSystem.IsFreeBSD())
+                    {
+                        File.SetUnixFileMode(p, UnixFileMode.GroupRead |
+                            UnixFileMode.GroupWrite |
+                            UnixFileMode.GroupExecute |
+                            UnixFileMode.UserRead |
+                            UnixFileMode.UserWrite |
+                            UnixFileMode.UserExecute |
+                            UnixFileMode.OtherRead |
+                            UnixFileMode.OtherWrite);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Debug(e.ToString());
+                }
+
+                try
+                {
+                    var dataFiles = Directory.GetFiles(targetResourcesPath);
+
+                    foreach(var file in dataFiles)
+                    {
+                        File.Move(file, Path.Combine(appPath, "Contents", "MacOS", "Data", Path.GetFileName(file)), true);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Log.Debug(e.ToString());
+                }
+
+                try
+                {
+                    var frameworks = Directory.GetFiles(outPath, "*.dylib")
+                        .Concat(Directory.GetFiles(outPath, "*.dll"));
+
+                    foreach(var framework in frameworks)
+                    {
+                        File.Move(framework, Path.Combine(appPath, "Contents", "MacOS", Path.GetFileName(framework)), true);
+                    }
+
+                    File.Move(Path.Combine(outPath, appName), Path.Combine(appPath, "Contents", "MacOS", appName), true);
+                }
+                catch(Exception e)
+                {
+                    Log.Debug(e.ToString());
+                }
             }
 
             lock (backgroundLock)
