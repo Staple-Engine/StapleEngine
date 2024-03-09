41b298ad67c7800b0258da2f3cfccda3c83b1a66
Nuno Silva <little.coding.fox@gmail.com>
2024-03-09 13:45:51 +0000

    [CI] Fix to build_windows;

--- Engine/build_windows.cmd
+++ Engine/build_windows.cmd
@@ -1,3 +1,9 @@
 @echo off
+
+rem due to a strange bug with dotnet, it won't restore properly the first time.
+rem so we restore twice...
 dotnet workload restore
-dotnet build Engine.sln -c Release -o ../Staging
+dotnet workload restore
+
+dotnet build Engine.sln -c Debug
+dotnet build Engine.sln -c Release
