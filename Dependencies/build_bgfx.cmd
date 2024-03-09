c2d6c7d23a958c7a72fd6dcd3326016ac483882d
Nuno Silva <little.coding.fox@gmail.com>
2024-03-09 15:54:45 +0000

    [Dependencies] Fix for windows;

--- Dependencies/build_bgfx.cmd
+++ Dependencies/build_bgfx.cmd
@@ -8,7 +8,7 @@ devenv .build\projects\vs2022\bgfx.sln  /Build "Debug|x64"
 
 devenv .build\projects\vs2022\bgfx.sln  /Build "Release|x64"
 
-mkdir ../../Tools/bin
+mkdir ..\..\Tools\bin
 
 copy /B /Y .build/win64_vs2022/bin/*cRelease ../../Tools/bin
 
