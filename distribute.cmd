69fa22af4e3917df7edac0f7bac2f8af37402bbc
Nuno Silva <little.coding.fox@gmail.com>
2024-03-09 20:11:39 +0000

    [CI] Debugging distribute;

--- distribute.cmd
+++ distribute.cmd
@@ -2,31 +2,53 @@
 
 if exist dist\ rmdir /S /Q dist
 
+echo %ERRORLEVEL%
+
 mkdir dist\StapleEngine
+
+echo %ERRORLEVEL%
 
 echo copy DefaultResources
 robocopy DefaultResources dist\StapleEngine\DefaultResources /E /NFL /NDL /NJH /NJS /NP /NS /NC
 
+echo %ERRORLEVEL%
+
 echo copy Editor
 robocopy Staging dist\StapleEngine\Editor /E /NFL /NDL /NJH /NJS /NP /NS /NC
+
+echo %ERRORLEVEL%
 
 echo copy Tools
 robocopy Tools\bin dist\StapleEngine\Tools\bin /E /NFL /NDL /NJH /NJS /NP /NS /NC
 
+echo %ERRORLEVEL%
+
 echo copy Shader Includes
 robocopy Tools\ShaderIncludes dist\StapleEngine\Tools\ShaderIncludes /E /NFL /NDL /NJH /NJS /NP /NS /NC
+
+echo %ERRORLEVEL%
 
 echo delete android compiled files
 rmdir /S /Q dist\StapleEngine\DefaultResources\Android
 
+echo %ERRORLEVEL%
+
 echo delete iOS compiled files
 rmdir /S /Q dist\StapleEngine\DefaultResources\iOS
+
+echo %ERRORLEVEL%
 
 echo delete linux compiled files
 rmdir /S /Q dist\StapleEngine\DefaultResources\Linux
 
+echo %ERRORLEVEL%
+
 echo delete macOSX compiled files
 rmdir /S /Q dist\StapleEngine\DefaultResources\MacOSX
 
+echo %ERRORLEVEL%
+
 echo delete windows compiled files
 rmdir /S /Q dist\StapleEngine\DefaultResources\Windows
+
+echo %ERRORLEVEL%
