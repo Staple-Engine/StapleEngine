f469698a31bea6b75289d2ab36d116168e0f5e23
Nuno Silva <little.coding.fox@gmail.com>
2024-03-09 10:50:58 +0000

    [CI] Fix build_windows.cmd;

--- Dependencies/build_windows.cmd
+++ Dependencies/build_windows.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-call C:\premake\premake5 vs2022
-call C:\premake\premake5 --file=NativeFileDialog/build/premake5.lua vs2022
-call C:\premake\premake5 --file=premake5_dotnet.lua vs2022
+call premake5 vs2022
+call premake5 --file=NativeFileDialog/build/premake5.lua vs2022
+call premake5 --file=premake5_dotnet.lua vs2022
 
 call build_bgfx.cmd
 
