efb45abb55a07e6adb7d618a5f61f50851c5b437
Nuno Silva <little.coding.fox@gmail.com>
2024-04-04 22:01:42 +0100

    [Editor] Added Custom Unity-Style Component asset template;

---
+++ Staging/Editor Resources/AssetTemplates/Custom Unity-Style Component.cs
@@ -0,0 +1,14 @@
+﻿using Staple;
+
+public class MyComponent : CallbackComponent
+{
+	//Called one frame after the component is added
+	public override void Start()
+	{
+	}
+	
+	//Called once a frame
+	public override void Update()
+	{
+	}
+}
