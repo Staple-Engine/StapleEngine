8abed0eb6487fc9341ef7fd2777954bfed5a640c
Nuno Silva <little.coding.fox@gmail.com>
2024-06-02 15:43:17 +0100

    [Dependencies] Attempt to fix Android build;

--- Dependencies/build_android_dependencies.sh
+++ Dependencies/build_android_dependencies.sh
@@ -11,18 +11,22 @@ cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cma
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Debug -S build/native -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native -G "Unix Makefiles"
 
-cd build/native/freetype/Debug
+cd build/native
+
+make -j $(nproc)
+
+cd ../../
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Release -S build/native -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native -G "Unix Makefiles"
+
+cd build/native
+
+make -j $(nproc)
+
+cd ../freetype/Debug
 
 make -j $(nproc)
 
 cd ../Release
-
-make -j $(nproc)
-
-cd ../..
-
-make -j $(nproc)
-
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_BUILD_TYPE=Release -S build/native -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native -G "Unix Makefiles"
 
 make -j $(nproc)
