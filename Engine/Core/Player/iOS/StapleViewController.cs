a15214edb97e179a4a014032951e32ad2465a2d5
Nuno Silva <little.coding.fox@gmail.com>
2025-04-21 12:27:46 +0100

    [Player] Attempt to fix iOS builds;

--- Engine/Core/Player/iOS/StapleViewController.cs
+++ Engine/Core/Player/iOS/StapleViewController.cs
@@ -77,7 +77,7 @@ public class StapleViewController : UIViewController
             }
 
             iOSRenderWindow.Instance.metalView = View as MetalView;
-            iOSRenderWindow.Instance.refreshRate = UIScreen.MainScreen.MaximumFramesPerSecond;
+            iOSRenderWindow.Instance.refreshRate = (int)UIScreen.MainScreen.MaximumFramesPerSecond;
 
             if (AppPlayer.instance?.renderWindow == null)
             {
