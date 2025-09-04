e5d551278d2de01156578400d88ae666b3a0388a
Nuno Silva <little.coding.fox@gmail.com>
2025-09-04 22:34:11 +0100

    [General] Update distribute scripts;

--- distribute.cmd
+++ distribute.cmd
@@ -8,7 +8,7 @@ robocopy DefaultResources dist\StapleEngine\DefaultResources /E /NFL /NDL /NJH /
 robocopy Staging dist\StapleEngine\Editor /E /NFL /NDL /NJH /NJS /NP /NS /NC
 robocopy Tools\bin dist\StapleEngine\Tools\bin /E /NFL /NDL /NJH /NJS /NP /NS /NC
 robocopy Tools\ShaderIncludes dist\StapleEngine\Tools\ShaderIncludes /E /NFL /NDL /NJH /NJS /NP /NS /NC
-robocopy Engine\EditorApp\bin\Release\net9.0\runtimes dist\StapleEngine\Editor\runtimes /E /NFL /NDL /NJH /NJS /NP /NS /NC
+robocopy Engine\Staple.Editor.App\bin\Release\net9.0\runtimes dist\StapleEngine\Editor\runtimes /E /NFL /NDL /NJH /NJS /NP /NS /NC
 robocopy "Staging\PlayerBackends\Windows\Redist\Release" dist\StapleEngine\Editor /E /NFL /NDL /NJH /NJS /NP /NS /NC
 
 rmdir /S /Q dist\StapleEngine\DefaultResources\Android
--- distribute.sh
+++ distribute.sh
@@ -14,7 +14,7 @@ cp -Rf DefaultResources dist/StapleEngine/
 cp -Rf Staging/* dist/StapleEngine/Editor/
 cp -Rf Tools/bin/* dist/StapleEngine/Tools/bin/
 cp -Rf Tools/ShaderIncludes/* dist/StapleEngine/Tools/ShaderIncludes/
-cp -Rf Engine/EditorApp/bin/Release/net9.0/runtimes dist/StapleEngine/Editor/runtimes
+cp -Rf Engine/Staple.Editor.App/bin/Release/net9.0/runtimes dist/StapleEngine/Editor/runtimes
 cp -Rf Staging/PlayerBackends/Linux/Redist/Release/* dist/StapleEngine/Editor/
 
 rm -Rf dist/StapleEngine/DefaultResources/Android
