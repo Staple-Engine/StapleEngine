922dcb1cb51c67278f0dbc0ee7ab51ad56498627
Nuno Silva <little.coding.fox@gmail.com>
2025-09-13 17:10:57 +0100

    [Dependencies] Fixes for android dependencies cmake flags;

--- Dependencies/build_android_dependencies.sh
+++ Dependencies/build_android_dependencies.sh
@@ -5,15 +5,15 @@ export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 premake5 --os=android --file=premake5_android.lua cmake
 premake5 --os=android --file=premake5_dotnet.lua vs2022
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/joltc -DCMAKE_BUILD_TYPE=Distribution -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S joltc -G "Unix Makefiles"
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/joltc -DCMAKE_BUILD_TYPE=Release -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S joltc -G "Unix Makefiles"
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/openal-soft/Debug -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S openal-soft -G "Unix Makefiles"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/openal-soft/Release -DCMAKE_BUILD_TYPE=Distribution -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S openal-soft -G "Unix Makefiles"
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/openal-soft/Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S openal-soft -G "Unix Makefiles"
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/freetype/Debug -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S freetype -G "Unix Makefiles"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/freetype/Release -DCMAKE_BUILD_TYPE=Distribution -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S freetype -G "Unix Makefiles"
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=26 -DANDROID_ABI=arm64-v8a -DCMAKE_INSTALL_PREFIX:String="Sdk" -B build/native/freetype/Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=true -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-z,max-page-size=16384" -S freetype -G "Unix Makefiles"
 
 cd build/native/joltc
 
