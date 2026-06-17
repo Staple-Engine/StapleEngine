cbd65c791c496bff7e22f543a44200b36789dc85
Nuno Silva <little.coding.fox@gmail.com>
2026-06-17 01:02:12 +0100

    [Tools] Fixes for linux and macOS file copying;

--- Tools/build_linux.sh
+++ Tools/build_linux.sh
@@ -3,6 +3,6 @@
 dotnet build Tools.sln -c Release -o bin
 
 cp ../Dependencies/build/native/bin/Release/libStapleToolingSupport.so bin/
-cp -R ../Dependencies/SDL_shadercross/Linux/* bin/
-cp -R ../Dependencies/slang/Linux/* bin/
-cp -R ../Dependencies/cuttlefish/Linux/* bin/
+cp -R ../Dependencies/SDL_shadercross/* bin/
+cp -R ../Dependencies/slang/* bin/
+cp -R ../Dependencies/cuttlefish/* bin/
--- Tools/build_macos.sh
+++ Tools/build_macos.sh
@@ -3,6 +3,6 @@
 dotnet build Tools.sln -c Release -o bin
 
 cp ../Dependencies/build/native/bin/Release/libStapleToolingSupport.dylib bin/
-cp -R ../Dependencies/SDL_shadercross/MacOSX/* bin/
-cp -R ../Dependencies/slang/MacOSX/* bin/
-cp -R ../Dependencies/cuttlefish/MacOSX/* bin/
+cp -R ../Dependencies/SDL_shadercross/* bin/
+cp -R ../Dependencies/slang/* bin/
+cp -R ../Dependencies/cuttlefish/* bin/
